f984dd86d6c5edf14801f746e6c806a031faa119
metabaronespinosa <axnotliztac@gmail.com>
2022-11-23 10:22:06 -0600

    (withdraw) use typechain methods

--- src/withdrawV0.ts
+++ src/withdrawV0.ts
@@ -12,20 +12,10 @@ export const withdrawFundsV0 = async (revPathAddress: string, walletAddress: str
   const contract = PathLibraryV0__factory.connect(revPathAddress, signer)
   
   try {
-    let tx
-
-    if (isERC20) {
-      const withdraw = contract.functions['release(address,address)']
-      
-      tx = await withdraw(ERC20s[isERC20], walletAddress)
-    } else {
-      const withdraw = contract.functions['release(address)']
-
-      tx = await withdraw(walletAddress)
-    }
-
+    const tx = isERC20 ?
+      await contract['release(address,address)'](ERC20s[isERC20], walletAddress) :
+      await contract['release(address)'](walletAddress)
     const result = await tx?.wait()
-    console.log("AAAA:::.", result)
     const [event] = result?.events || [{ args: [] }]
 
     console.log("Withdraw result:::...", event?.args && ethers.utils.formatEther(event?.args[1]))
--- src/withdrawV1.ts
+++ src/withdrawV1.ts
@@ -12,18 +12,9 @@ export const withdrawFundsV1 = async (revPathAddress: string, walletAddress: str
   const contract = PathLibraryV1__factory.connect(revPathAddress, signer)
   
   try {
-    let tx
-
-    if (isERC20) {
-      const withdraw = contract.functions.releaseERC20
-
-      tx = await withdraw(ERC20s[isERC20], walletAddress)
-    } else {
-      const withdraw = contract.functions.release
-
-      tx = await withdraw(walletAddress)
-    }
-
+    const tx = isERC20 ?
+      await contract.releaseERC20(ERC20s[isERC20], walletAddress) :
+      await contract.release(walletAddress)
     const result = await tx?.wait()
     const [event] = result?.events || [{ args: [] }]
     
