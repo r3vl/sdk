01a8d1889e63055d6ecea5950e76e0e95edf9631
metabaronespinosa <axnotliztac@gmail.com>
2022-11-24 19:30:15 -0600

    (withdrawable) implement erc20 for V0

--- src/eth-sdk/config.ts
+++ src/eth-sdk/config.ts
@@ -6,11 +6,13 @@ export default defineConfig({
       reveelMainV1: '0x320becCdfbed31a84A0c6E6F678320814B400BBe',
       pathLibraryV1: '0x9C23D192313b60225459bee71BC96cF49740418E',
       reveelMainV0: '0x1b5aa0D926ea3c4C4485Ba447c9ABd9A5A128967',
-      pathLibraryV0: '0x92582A5091A55A8Cd168A927ec100fDD13Ec0f20'
+      pathLibraryV0: '0x92582A5091A55A8Cd168A927ec100fDD13Ec0f20',
+      weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     },
     goerli: {
       reveelMain: '0xff960959d00DbbaB1096C3A9a13E81e239d1c374',
-      pathLibraryV0: '0xc66d1E8d7337b60d8cf00850674FB6E1eE73D985'
+      pathLibraryV0: '0xc66d1E8d7337b60d8cf00850674FB6E1eE73D985',
+      weth: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'
     }
   },
 })
--- src/withdrawableV0.ts
+++ src/withdrawableV0.ts
@@ -3,39 +3,44 @@ import { ethers } from 'ethers'
 import { PathLibraryV0__factory } from './typechain'
 import { tokenList } from "./constants/tokens"
 import { communityProvider } from './utils'
+import { getMainnetSdk, getGoerliSdk } from '@dethcrypto/eth-sdk-client'
 
 
 /**
  *  V0
  */
-export const withdrawableV0 = async (revPathAddress: string, walletAddress: string, isERC20?: keyof typeof tokenList) => {
+export const withdrawableV0 = async (revPathAddress: string, walletAddress: string, isERC20?: 'weth') => {
   const provider = communityProvider()
+  const { chainId } = await provider.getNetwork()
 
   try {
     const revPath = PathLibraryV0__factory.connect(revPathAddress, provider)
-    const withdraws = await revPath.queryFilter(
-      revPath.filters.PaymentReleased()
-    )
-    const paymentsReceived = withdraws
-      .filter(ev => {
-        const [wallet] = ev?.args || [""]
 
-        return wallet === walletAddress
-      })
-      .reduce((prev, curr: { args: any[] }) => prev + parseFloat(ethers.utils.formatEther(curr?.args[1])), 0)
+    const walletShare = await revPath.shares(walletAddress)
+    
+    if (isERC20) {
+      const sdk = getMainnetSdk(provider)
+      
+      const totalReleased = await revPath['totalReleased(address)'](tokenList[isERC20][chainId])
+      const totalReceived = await sdk[isERC20].balanceOf(revPathAddress)
+      const totalAccounted = parseFloat(ethers.utils.formatEther(totalReceived)) + parseFloat(ethers.utils.formatEther(totalReleased))
+      
+      return (totalAccounted * walletShare.toNumber() / 10000) - parseFloat(ethers.utils.formatEther(totalReleased))
+    }
 
+    const totalReleased = await revPath['totalReleased()']()
     const deposits = await revPath.queryFilter(
       revPath.filters.PaymentReceived()
     )
-    const depositsReceived = deposits
+    const totalReceived = deposits
       .filter(ev => {
         const [wallet] = ev?.args || [""]
 
         return wallet === walletAddress
       })
       .reduce((prev, curr: { args: any[] }) => prev + parseFloat(ethers.utils.formatEther(curr?.args[1])), 0)
-
-    return depositsReceived - paymentsReceived
+    
+    return (totalReceived * walletShare.toNumber() / 10000) - parseFloat(ethers.utils.formatEther(totalReleased))
   } catch (error) {
     console.error(error)
   }
