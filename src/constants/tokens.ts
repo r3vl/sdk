3263c11fad2f11f1496acbce14bd5b316f992e35
urartiandev <armen.orr@protonmail.com>
2023-02-16 16:56:01 +0400

    added matic & maticMumbai correct addresses

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -41,6 +41,11 @@ export const tokenList = {
     [chainIds.goerli]: "0x0000000000000000000000000000000000000000",
     [chainIds.mainnet]: "0x0000000000000000000000000000000000000000",
   },
+  matic: {
+    name: "Matic",
+    [chainIds.polygon]: "0x0000000000000000000000000000000000001010",
+    [chainIds.polygonMumbai]: '0xca11bde05977b3631167028862be2a173976ca11'
+  },
   weth: {
     name: "Wrapped Ether",
     [chainIds.goerli]: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
@@ -50,6 +55,14 @@ export const tokenList = {
     name: "DAI",
     [chainIds.goerli]: "0x41e38e70a36150D08A8c97aEC194321b5eB545A5",
     [chainIds.mainnet]: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    [chainIds.polygon]: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+    [chainIds.optimism]: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
+    [chainIds.arbitrum]: '0xda10009cbd5d07dd0cecc66161fc93d7c9000da1',
+    [chainIds.aurora]: '0xe3520349f477a5f6eb06107066048508498a291b',
+    [chainIds.polygonMumbai]: '0xd393b1E02dA9831Ff419e22eA105aAe4c47E1253',
+    [chainIds.optimismGoerli]: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+    [chainIds.arbitrumGoerli]: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    [chainIds.auroraTestnet]: "0xe3520349F477A5F6EB06107066048508498A291b",
   },
   usdc: {
     name: "USD Coin",
